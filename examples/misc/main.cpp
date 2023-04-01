0e1173fb42ed571838745ce224a4554a3fcb8afe
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-04-01 12:08:27 +0800

    Add files via upload

--- examples/misc/main.cpp
+++ examples/misc/main.cpp
@@ -5,38 +5,53 @@
 class MyEnum : public ntl::Enumeration
 {
 public:
-    using EnumerationType = ntl::Enumeration;
+    using EnumType = ntl::Enumeration;
 
 public:
-    static EnumerationType A;
-    static EnumerationType B;
-    static EnumerationType C;
+    static const EnumType A;
+    static const EnumType B;
+    static const EnumType C;
 };
 
-MyEnum::EnumerationType MyEnum::A = EnumerationType(0);
-MyEnum::EnumerationType MyEnum::B = EnumerationType(1);
-MyEnum::EnumerationType MyEnum::C = EnumerationType(2);
+const MyEnum::EnumType MyEnum::A = EnumType(0);
+const MyEnum::EnumType MyEnum::B = EnumType(1);
+const MyEnum::EnumType MyEnum::C = EnumType(2);
 
 class MyEnum2 : public MyEnum
 {
 public:
-    static EnumerationType D;
-    static EnumerationType E;
-    static EnumerationType F;
+    static const EnumType D;
+    static const EnumType E;
+    static const EnumType F;
 };
 
-MyEnum2::EnumerationType MyEnum2::D = EnumerationType(3);
-MyEnum2::EnumerationType MyEnum2::E = EnumerationType(4);
-MyEnum2::EnumerationType MyEnum2::F = EnumerationType(5);
+const MyEnum2::EnumType MyEnum2::D = EnumType(3);
+const MyEnum2::EnumType MyEnum2::E = EnumType(4);
+const MyEnum2::EnumType MyEnum2::F = EnumType(5);
+
+namespace MyEnum3
+{
+    using EnumType = ntl::StringEnumeration;
+
+    static const EnumType A = EnumType("A");
+    static const EnumType B = EnumType("B");
+    static const EnumType C = EnumType("C");
+
+} // namespace MyEnum3
 
 int main()
 {
     std::cout << "MyEnum2::A:\t" << MyEnum2::A << std::endl
               << "MyEnum2::B:\t" << MyEnum2::B << std::endl
               << "MyEnum2::C:\t" << MyEnum2::C << std::endl
+              << std::endl
               << "MyEnum2::D:\t" << MyEnum2::D << std::endl
               << "MyEnum2::E:\t" << MyEnum2::E << std::endl
-              << "MyEnum2::F:\t" << MyEnum2::F << std::endl;
+              << "MyEnum2::F:\t" << MyEnum2::F << std::endl
+              << std::endl
+              << "MyEnum3::A:\t" << MyEnum3::A << std::endl
+              << "MyEnum3::B:\t" << MyEnum3::B << std::endl
+              << "MyEnum3::C:\t" << MyEnum3::C << std::endl;
 
     return 0;
 }
