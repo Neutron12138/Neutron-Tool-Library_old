c636d5f92cf45e73d2de268f33999085bf7f29af
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-04-01 12:08:06 +0800

    Add files via upload

--- src/Misc/Enumeration.cpp
+++ src/Misc/Enumeration.cpp
@@ -27,9 +27,49 @@ namespace ntl
     }
 
     template <typename m_ValueType>
+    typename BasicEnumeration<m_ValueType>::SelfType
+    BasicEnumeration<m_ValueType>::operator|(
+        const typename BasicEnumeration<m_ValueType>::SelfType &another) const
+    {
+        return SelfType(m_value | another.m_value);
+    }
+
+    template <typename m_ValueType>
+    typename BasicEnumeration<m_ValueType>::SelfType
+    BasicEnumeration<m_ValueType>::operator&(
+        const typename BasicEnumeration<m_ValueType>::SelfType &another) const
+    {
+        return SelfType(m_value & another.m_value);
+    }
+
+    template <typename m_ValueType>
+    typename BasicEnumeration<m_ValueType>::SelfType &
+    BasicEnumeration<m_ValueType>::operator|=(
+        const typename BasicEnumeration<m_ValueType>::SelfType &another)
+    {
+        *this = *this | another;
+        return *this;
+    }
+
+    template <typename m_ValueType>
+    typename BasicEnumeration<m_ValueType>::SelfType &
+    BasicEnumeration<m_ValueType>::operator&=(
+        const typename BasicEnumeration<m_ValueType>::SelfType &another)
+    {
+        *this = *this & another;
+        return *this;
+    }
+
+    template <typename m_ValueType>
     BasicEnumeration<m_ValueType>::operator BasicEnumeration<m_ValueType>::ValueType() const
     {
         return m_value;
+    }
+
+    template <typename m_ValueType>
+    BasicEnumeration<m_ValueType>::operator bool() const
+    {
+        return !is_zero();
     }
 
     template <typename m_ValueType>
@@ -37,6 +77,60 @@ namespace ntl
     BasicEnumeration<m_ValueType>::get_value() const
     {
         return m_value;
+    }
+
+    template <typename m_ValueType>
+    bool
+    BasicEnumeration<m_ValueType>::is_zero() const
+    {
+        return m_value == 0;
+    }
+
+    //
+    // 模板特化
+    //
+
+    BasicEnumeration<String>::BasicEnumeration(
+        const typename BasicEnumeration<String>::ValueType &value)
+        : m_value(value) {}
+
+    bool
+    BasicEnumeration<String>::operator==(
+        const typename BasicEnumeration<String>::SelfType &another) const
+    {
+        return m_value == another.m_value;
+    }
+
+    bool
+    BasicEnumeration<String>::operator!=(
+        const typename BasicEnumeration<String>::SelfType &another) const
+    {
+        return !operator==(another);
+    }
+
+    BasicEnumeration<String>::operator BasicEnumeration<String>::ValueType() const
+    {
+        return m_value;
+    }
+
+    const typename BasicEnumeration<String>::ValueType &
+    BasicEnumeration<String>::get_value() const
+    {
+        return m_value;
+    }
+
+    //
+    // 重载函数
+    //
+
+    template <typename ValueType>
+    OutputStream &
+    operator<<(
+        OutputStream &os,
+        const BasicEnumeration<ValueType> &enumeration)
+    {
+        os << enumeration.get_value();
+        return os;
     }
 
 } // namespace ntl
--- src/Misc/Enumeration.hpp
+++ src/Misc/Enumeration.hpp
@@ -28,8 +28,61 @@ namespace ntl
 
     public:
         SelfType &operator=(const SelfType &from) = default;
+
         bool operator==(const SelfType &another) const;
         bool operator!=(const SelfType &another) const;
+
+        SelfType operator|(const SelfType &another) const;
+        SelfType operator&(const SelfType &another) const;
+
+        SelfType &operator|=(const SelfType &another);
+        SelfType &operator&=(const SelfType &another);
+
+        operator ValueType() const;
+        operator bool() const;
+
+    public:
+        /// @brief 获取值
+        /// @return 值
+        const ValueType &get_value() const;
+
+        /// @brief 值是否为0
+        /// @return 值是否为0
+        bool is_zero() const;
+    };
+
+    /// @brief 枚举
+    using Enumeration = BasicEnumeration<Int64>;
+
+    //
+    // 模板特化
+    //
+
+    /// @brief 字符串枚举
+    template <>
+    class BasicEnumeration<String> : public BasicObject
+    {
+    public:
+        using ValueType = String;
+
+        using SelfType = BasicEnumeration<ValueType>;
+        using ParentType = BasicObject;
+
+    protected:
+        /// @brief 值
+        ValueType m_value;
+
+    public:
+        BasicEnumeration(const ValueType &value);
+        explicit BasicEnumeration(const SelfType &from) = default;
+        ~BasicEnumeration() = default;
+
+    public:
+        SelfType &operator=(const SelfType &from) = default;
+
+        bool operator==(const SelfType &another) const;
+        bool operator!=(const SelfType &another) const;
+
         operator ValueType() const;
 
     public:
@@ -38,8 +91,15 @@ namespace ntl
         const ValueType &get_value() const;
     };
 
-    /// @brief 枚举
-    using Enumeration = BasicEnumeration<UInt64>;
+    /// @brief 字符串枚举
+    using StringEnumeration = BasicEnumeration<String>;
+
+    //
+    // 重载函数
+    //
+
+    template <typename ValueType>
+    OutputStream &operator<<(OutputStream &os, const BasicEnumeration<ValueType> &enumeration);
 
 } // namespace ntl
 
