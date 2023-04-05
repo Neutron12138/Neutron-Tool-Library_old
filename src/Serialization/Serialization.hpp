62e3f7dcc12267e711e2e7d636e3fad1cd7464c8
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-04-05 09:21:23 +0800

    Add files via upload

--- src/Serialization/Serialization.hpp
+++ src/Serialization/Serialization.hpp
@@ -1,17 +1,17 @@
 #ifndef __NEUTRONTL_SERIALIZATION_SERIALIZATION_HPP__
 #define __NEUTRONTL_SERIALIZATION_SERIALIZATION_HPP__
 
-#include "../Base/Comparable.hpp"
+#include "../Base/Object.hpp"
 #include "Serializable.hpp"
 
 namespace ntl
 {
     /// @brief 序列
-    class NTL_ALIGN Serialization : public Comparable
+    class NTL_ALIGN Serialization : public Object
     {
     public:
         using SelfType = Serialization;
-        using ParentType = Comparable;
+        using ParentType = Object;
 
     protected:
         /// @brief 序列
@@ -21,16 +21,16 @@ namespace ntl
         Serialization() = default;
         explicit Serialization(const String &serialization);
         explicit Serialization(const SelfType &from) = default;
-        ~Serialization() override = default;
+        ~Serialization() = default;
 
     public:
         SelfType &operator=(const SelfType &from) = default;
-        bool operator>(const SelfType &another) override;
-        bool operator<(const SelfType &another) override;
-        bool operator>=(const SelfType &another) override;
-        bool operator<=(const SelfType &another) override;
-        bool operator==(const SelfType &another) override;
-        bool operator!=(const SelfType &another) override;
+        bool operator>(const SelfType &another);
+        bool operator<(const SelfType &another);
+        bool operator>=(const SelfType &another);
+        bool operator<=(const SelfType &another);
+        bool operator==(const SelfType &another);
+        bool operator!=(const SelfType &another);
         operator String();
         operator String() const;
 
