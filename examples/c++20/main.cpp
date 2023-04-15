034f3af701bef18a99fd9d6dd676f5ed86c193a7
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-04-15 15:57:32 +0800

    Add files via upload

--- examples/c++20/main.cpp
+++ examples/c++20/main.cpp
@@ -3,6 +3,15 @@
 #include "../../src/NTL.hpp"
 #include "../../src/NTL.cpp"
 
+struct A
+{
+    A copy() { return A(); }
+    A &swap(const A &) { return *this; }
+};
+
+template <ntl::cpp20::CopyableType T>
+void func() {}
+
 int main()
 {
     std::cout << "std::string():"
@@ -11,6 +20,16 @@ int main()
               << ntl::cpp20::can_construct<std::string, const char *> << std::endl
               << "std::string(int):"
               << ntl::cpp20::can_construct<std::string, int> << std::endl;
+
+    std::cout << "----------" << std::endl;
+
+    std::cout << "ntl::cpp20::CopyableType<A>:"
+              << ntl::cpp20::CopyableType<A> << std::endl
+              << "ntl::cpp20::CopyableType<int>:"
+              << ntl::cpp20::CopyableType<int> << std::endl;
+
+    func<A>();
+    // func<int>(); 编译失败
 
     return 0;
 }
