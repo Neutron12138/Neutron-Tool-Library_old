02b6f55ea99b2281a2babb7e181a1a2a82fe3d01
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-05-06 21:17:13 +0800

    Add files via upload

--- examples/debug/main.cpp
+++ examples/debug/main.cpp
@@ -3,10 +3,28 @@
 #include "../../src/NTL.hpp"
 #include "../../src/NTL.cpp"
 
+void test();
+
 int main()
 {
     NTL_set_debug_output([](const char *str)
                          { std::cout << str << std::endl; });
+
     NTL_debug_output("abc");
+
+    std::cout << "----------" << std::endl;
+
+    NTL_debug("int main()", test(););
+
+    std::cout << "----------" << std::endl;
+
+    NTL_debug("int main()", throw 666;);
+
     return 0;
+}
+
+void test()
+{
+    std::array<int, 0> a;
+    NTL_debug_throw("void test()", a.at(0););
 }
